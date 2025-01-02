e41960ee75f50a1074321f89cf1366dc30a11140
Alfred Palomares <apalomares161@gmail.com>
2025-01-02 10:37:10 +0800

    Initial commit for all components

---
+++ src/components/AllUsersTab/AllUsersTab.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AllUsersTab = props => {
+  return <div>AllUsersTab</div>;
+};
+
+AllUsersTab.propTypes = {};
+
+export default AllUsersTab;
